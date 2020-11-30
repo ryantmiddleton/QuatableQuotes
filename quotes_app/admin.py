237c24dd984800294c78cd697550471dd4888141
Ryan Middleton <ryanTmiddleton@gmail.com>
2020-11-30 08:56:47 -0800

    added to .gitignore

--- quotes_app/admin.py
+++ quotes_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from quotes_app.models import User, Quote, Like
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(User, UserAdmin)
+
+class QuoteAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Quote, QuoteAdmin)
+
+class LikeAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Like, LikeAdmin)
